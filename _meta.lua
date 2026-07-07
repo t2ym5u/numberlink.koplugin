0e1244030b38d82e7658016c80ac1f110ff675da
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-07 13:52:51 +0200

    chore: bump version to 1.0.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "numberlink",
     fullname    = _("Numberlink"),
     description = _("Connect each pair of matching numbers with a path covering every cell."),
-    version     = "1.0.0",
+    version     = "1.0.1",
 }
